3cc9bc69769c188edc52baa93e590b68ea20588b
Ameer Muhammed MM <ami.mullan@gmail.com>
2020-06-01 21:40:27 +0530

    validator

--- src/controllers/controller.js
+++ src/controllers/controller.js
@@ -7,10 +7,9 @@ exports.addQuestions = async (req,res) => {
     await validateQuestion(req.body)
     .then(async ()=>{
             const question = new Question({ question: req.body.question, option1: req.body.option1, option2: req.body.option2, option3: req.body.option3, option4: req.body.option4, answer: req.body.answer })
-            await question.save().then((doc)=>{return res.send(doc)}).catch((err)=>{return res.send(err)})
+            await question.save().then((doc)=>{return res.send({"method":"post","status":"success",doc})}).catch((err)=>{return res.send(err)})
         })
     .catch((err)=>{return res.send(err)})
-
 }
 
 exports.updateQuestion = async(req,res) =>{
@@ -18,7 +17,7 @@ exports.updateQuestion = async(req,res) =>{
     .then (async()=>{
         await Question.findByIdAndUpdate({_id: req.body._id}, {$set:{question: req.body.question, option1: req.body.option1, option2: req.body.option2, option3: req.body.option3, option4: req.body.option4, answer: req.body.answer}}, {new: true}, (err, doc) => {
             if (err) return res.send({"Error":"502: Something went wrong!"})
-            return res.send(doc) })
+            return res.send({"method":"put","status":"success",doc}) })
     })
     .catch((err)=>{return res.send(err)}) 
 }
@@ -26,5 +25,5 @@ exports.updateQuestion = async(req,res) =>{
 exports.deleteQuestion = async(req,res) =>{
         await Question.findByIdAndDelete({_id: req.body._id}, (err, doc) => {
             if (err) return res.send({"Error":"502: Something went wrong!"})
-            return res.send(doc) })
+            return res.send({"method":"delete","status":"success",doc}) })
 }
