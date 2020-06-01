b39392f0a681f43084c67bacbb9bb7387de6ec11
Ameer Muhammed MM <ami.mullan@gmail.com>
2020-06-01 21:17:21 +0530

    validator

--- src/controllers/controller.js
+++ src/controllers/controller.js
@@ -1,12 +1,5 @@
 const Question = require('../models/Question')
-
-const validateQuestion = (body) => {
-    return new Promise((resolve, reject) => {
-            if(typeof body.question == 'undefined' || typeof body.option1 == 'undefined' || typeof body.option2 == 'undefined' || typeof body.option3 == 'undefined' || typeof body.option4 == 'undefined' || typeof body.answer == 'undefined') return reject({"Error":"400:Bad Request. All fields mandatory!"})
-            body.question=body.question.trim(); body.option1=body.option1.trim(); body.option2=body.option2.trim(); body.option3=body.option3.trim(); body.option4=body.option4.trim(); body.answer=body.answer.trim()
-            if(!(body.answer==body.option1 || body.answer==body.option2  || body.answer==body.option3  || body.answer==body.option4 )) return reject({"Error":"400:Bad Request. Answer must be equal to any of the given options!"})
-            resolve() })
-}
+require('./validator')
 
 exports.getQuestions = async (req,res) => { return res.send(await Question.aggregate([{ $sample: { size: Number(req.params.count)>0? Number(req.params.count) : 1 } }])) }
 
